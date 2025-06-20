725da18e03edb398ec9aa6911b3484814d443807
Supanut Suntikoon (Boon) <vocabswipe@gmial.com>
2025-06-21 04:13:31 +0700

    .

--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-// D:\vocabswipe.github.io\next.config.js
-module.exports = {
-  basePath: '/vocabswipe.github.io',
-  assetPrefix: '/vocabswipe.github.io/',
-  output: 'export',
-  trailingSlash: true,
-};
