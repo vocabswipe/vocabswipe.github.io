4bb589a6902063517ef002c2555d4fef850a280b
vocabswipe <vocabswipe@gmail.com>
2025-06-21 03:56:17 +0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+// D:\vocabswipe.github.io\next.config.js
+module.exports = {
+  basePath: '/vocabswipe.github.io',
+  assetPrefix: '/vocabswipe.github.io/',
+  output: 'export',
+  trailingSlash: true,
+};
