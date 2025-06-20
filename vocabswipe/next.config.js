1d42cb594ecadd4e9f7af9a59f99340db26b8fdb
vocabswipe <vocabswipe@gmail.com>
2025-06-21 04:30:35 +0700

    Delete vocabswipe directory

--- vocabswipe/next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  basePath: '/vocabswipe.github.io',
-  assetPrefix: '/vocabswipe.github.io/',
-  output: 'export',
-  trailingSlash: true,
-};
--- vocabswipe/pages.index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold text-center">VocabSwipe</h1>
-      <p className="text-center mt-4">Welcome to English Flashcards</p>
-    </div>
-  );
-}
