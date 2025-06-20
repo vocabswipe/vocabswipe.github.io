d82f57aacdc5791ab333bedb5559aeb3a7122548
Supanut Suntikoon (Boon) <vocabswipe@gmial.com>
2025-06-21 04:13:51 +0700

    .

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-// D:\vocabswipe.github.io\pages\index.js
-export default function Home() {
-  return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold text-center">VocabSwipe</h1>
-      <p className="text-center mt-4">Welcome to English Flashcards</p>
-    </div>
-  );
-}
