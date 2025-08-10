9d37e5a5d892492173db312d1a76f13c07317c28
vocabswipe <vocabswipe@gmail.com>
2025-08-11 00:48:58 +0700

    Update .eslintrc.js

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,21 +1,21 @@
-{
-  "env": {
-    "node": true,
-    "es2021": true
+module.exports = {
+  env: {
+    node: true,
+    es2021: true
   },
-  "extends": [
+  extends: [
     "google"
   ],
-  "parserOptions": {
-    "ecmaVersion": 12,
-    "sourceType": "module"
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module"
   },
-  "rules": {
-    "quotes": ["error", "double"],
+  rules: {
+    quotes: ["error", "double"],
     "linebreak-style": ["error", "unix"],
-    "max-len": ["error", { "code": 100 }],
-    "indent": ["error", 2],
+    "max-len": ["error", { code: 100 }],
+    indent: ["error", 2],
     "object-curly-spacing": ["error", "always"],
     "comma-dangle": ["error", "always-multiline"]
   }
-}
+};
