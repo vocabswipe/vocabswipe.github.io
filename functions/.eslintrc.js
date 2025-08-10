0732034fdd539a21bfa756d6f794ac36f89df762
vocabswipe <vocabswipe@gmail.com>
2025-08-11 00:52:45 +0700

    Update .eslintrc.js

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
   env: {
     node: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
-    "google"
+    "google",
   ],
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: "module"
+    sourceType: "module",
   },
   rules: {
-    quotes: ["error", "double"],
+    "quotes": ["error", "double"],
     "linebreak-style": ["error", "unix"],
     "max-len": ["error", { code: 100 }],
-    indent: ["error", 2],
+    "indent": ["error", 2],
     "object-curly-spacing": ["error", "always"],
-    "comma-dangle": ["error", "always-multiline"]
-  }
+    "comma-dangle": ["error", "always-multiline"],
+  },
 };
